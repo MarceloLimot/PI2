fd72b0a3cda01c2b3a462e98d7b10632c4cffe1b
MarceloLimot <marcelolimot@gmail.com>
2022-06-30 22:08:53 -0300

    PI2(30-06)
    PI2(30-06)

--- projetopi2/projetopi2/settings.py
+++ projetopi2/projetopi2/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,6 +129,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/profissionais'
 
 LOGOUT_REDIRECT_URL = '/accounts/login'
+
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
